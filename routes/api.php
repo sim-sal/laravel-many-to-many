c841beb59eeb2f08a21c9aa01864b7a062a0ac2f
sim-sal <simone.saladino93@gmail.com>
2023-08-22 19:37:27 +0200

    create new Route in api.php

--- routes/api.php
+++ routes/api.php
@@ -3,17 +3,9 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+use App\Http\Controllers\Api\ProjectController;
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::prefix('v1')->group(function () {
+
+    Route::get('/projects', [ProjectController::class, 'projectsIndex']);
 });
